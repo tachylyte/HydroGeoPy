49e705e5f512213f50e243c80d8e0f9aea7146cb
tachylyte <chriscollinson@toughnode.com>
2017-01-13 13:00:46 +0000

    Can now be run as a script or imported

--- inversion.py
+++ inversion.py
@@ -1,10 +1,13 @@
-#N = 16
+#Module for carrying out a numerical inversion of Laplace transforms.  
+#There are functions to generate Stehfest coefficients and to implement the Stehfest inversion method
 
-def stehfest(N):    
-    '''Assume that N is an integer and N == 0
-    Normally, the optimum value of N is determined as a result of a numerical experiment. As a reference, however, the range of 6 ≤ N ≤ 18 covers the most common values of N for transient flow problems. 
-    http://petrowiki.org/Laplace_transformation_for_solving_transient_flow_problems
-    Return list with the Stehfest coefficients'''
+N = 16
+
+def stehfestCoeff(N):    
+    ''' Assume that N is an integer and N == 0
+        Normally, the optimum value of N is determined as a result of a numerical experiment. As a reference, however, the range of 6 ≤ N ≤ 18 covers the most common values of N for transient flow problems. 
+        http://petrowiki.org/Laplace_transformation_for_solving_transient_flow_problems
+        Return list with the Stehfest coefficients'''
     import math
     ln2 = math.log(2.0)
     N2 = N / 2 
@@ -24,6 +27,10 @@ def stehfest(N):
             V[i] = V[i] + (math.pow(k, N2) / math.factorial(k)) * (math.factorial(2 * k) / math.factorial(2 * k - i - 1)) / math.factorial(N2 - k) / math.factorial(k - 1) / math.factorial(i + 1 - k)
         V[i] = sign * V[i]
 
-    print(V)
+        
+if __name__ == "__main__":
+    import sys
+    print(stehfestCoeff(int(sys.argv[1])))
+    
 
-#stehfest(N)
+
