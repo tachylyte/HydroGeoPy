572b0d54fac6dfa35b0f07a8e3ff014769c27534
tachylyte <chriscollinson@toughnode.com>
2017-01-20 13:46:05 +0000

    added average K functions

--- helpers.py
+++ helpers.py
@@ -19,13 +19,44 @@ def velocity(distance, time):
     return velocity
 
 def aveVertK(*args):
-    '''Assume even number of 
-    aveVertK = 
-    return ###
+    '''Accepts multiple layers.  Need a hydraulic conductivity (K) and thickness (L) for each.
+    Specify all the K values before the L values e.g. if there are two layers the variables passed
+    to the function will be K1, K2, L1, L2.
+    Return the average vertical hydraulic conductivity.'''
+    numberArgs = len(args)
+    split = numberArgs // 2
+    listK = args[:split]
+    listL = args[split:]
+    if len(listK) != len(listL):
+        raise Exception('Unequal number of arguments')
+    sumThickness = 0
+    sumLdivK = 0
+    for t in listL:
+        sumThickness = sumThickness + t
+    for i in range(len(listK)):
+        sumLdivK += listL[i] / listK[i]
+    aveVertK = sumThickness / sumLdivK
+    return aveVertK
 
-def aveHorizK(####):
-    #####
-    return ###
+def aveHorizK(*args):
+    '''Accepts multiple layers.  Need a hydraulic conductivity (K) and thickness (L) for each.
+    Specify all the K values before the L values e.g. if there are two layers the variables passed
+    to the function will be K1, K2, L1, L2.
+    Return the average horizontal hydraulic conductivity.'''
+    numberArgs = len(args)
+    split = numberArgs // 2
+    listK = args[:split]
+    listL = args[split:]
+    if len(listK) != len(listL):
+        raise Exception('Unequal number of arguments')
+    sumThickness = 0
+    sumKL = 0
+    for t in listL:
+        sumThickness = sumThickness + t
+    for i in range(len(listK)):
+        sumKL += listL[i] * listK[i]
+    aveHorizK = sumKL / sumThickness 
+    return aveHorizK
 
 def retardation(###):
     #####
