e53f0824a059e3685cee34c38ee1650df71256f2
tachylyte <chriscollinson@toughnode.com>
2017-01-20 12:44:41 +0000

    Update helpers.py

--- helpers.py
+++ helpers.py
@@ -1,20 +1,26 @@
 # Useful helper functions
 
 def travelTime(velocity, distance):
-    '''Return time of travel for distance'''
+    '''Assume veolicty and distance provided.
+    Return time of travel for distance'''
     time = distance / velocity
     return time
 
 def distanceTravelled(velocity, time):
+    '''Assume velocity and time provided.
+    Return distance.'''
     distance = velocity * time
     return distance
 
 def velocity(distance, time):
+    '''Assume distance and time provided.
+    Return velocity.'''
     velocity = distance / time
     return velocity
 
-def aveVertK(##):
-    ######
+def aveVertK(*args):
+    '''Assume even number of 
+    aveVertK = 
     return ###
 
 def aveHorizK(####):
